6ccd769e75d825c837f2548f17a5e814a781b845
Jorge Chavez <ljorchavez@gmail.com>
2022-10-05 13:59:41 -0600

    changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import ReactMarkdown from 'react-markdown'
 import Header from "./Components/Header"
 import Menu from "./Components/Menu"
